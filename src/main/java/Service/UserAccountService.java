c43311d471db031298f4883d22a870a8df8a8c82
Tanya Costello <tanyahoo@gmail.com>
2025-04-13 01:48:26 +0000

    added method to create account in UserAccountService class

--- src/main/java/Service/UserAccountService.java
+++ src/main/java/Service/UserAccountService.java
@@ -16,39 +16,38 @@ public class UserAccountService {
 
 
 
-
-
-    // using UserAccountDAO to persist a new account to the database
-    // method checks if the account Id already exists, the registration will be successful if and only if the 
+    // method that uses UserAccountDAO to persist a new account to the database
+    // the registration will be successful if and only if the 
     // username is not blank, 
     // the password is at least 4 characters long, and an Account with that username does not already exist
-    // before it attempts to persist it
+    
     // @param acc an Account object.
     // @return account if it was successfully persisted, null if it was not successfully persisted
+
     public Account createAccount( Account acc){
-        int accountId = acc.getAccount_id();
-        
+        String user = acc.getUsername();
+        String pass = acc.getPassword();
 
-        
-        Account existingAccount = userDao.getAccountByAccountId(accountId);
-        if (existingAccount == null && ){
+        // check username and password for conditions
+         if (user == null || user.trim().isEmpty()) {
+             System.out.println("Username cannot be blank.");
+                return null;
+         }
+         if (pass == null || pass.length() < 4) {
+             System.out.println("Password must be at least 4 characters long.");
+                 return null;
+         }
 
+         // check if there's an account created with the username
+         Account existing = userDao.getAccountByUsername(user);
 
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return null;
+         // if this account exists then the username is in use
+        if (existing != null ){
+            System.out.println("Username already exists!");
+            return null;
+            }
+        // return account created with username and password that meet the requirements
+        return userDao.getAccountByUsernameAndPassword(user, pass);
     }
 
 
